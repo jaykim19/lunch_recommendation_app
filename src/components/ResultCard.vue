fba282102a1aeee98184f7b66267c86356b0f673
jaykim19 <nobletime@naver.com>
2026-05-19 10:40:14 +0900

    fixed a typo

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -11,7 +11,7 @@ defineEmits(["restart"]);
 
 <template>
   <section v-if="confirmedFood" class="result-card">
-    <h3>오늘의 점심은 {{ confirmedFood.name }}!</h3>
+    <h3>오늘의 식사는 {{ confirmedFood.name }}!</h3>
     <p>맛있게 먹고 오세요.</p>
     <!-- <button class="ghost-btn" @click="$emit('restart')">다시 하기</button> !-->
   </section>
